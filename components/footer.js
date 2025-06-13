721f19fe8a0f797b57da65abf36979ee60b7a83d
BearTony <18113020130@163.com>
2025-06-13 11:56:11 +0800

    add mcp sites in homepage

--- components/footer.js
+++ components/footer.js
@@ -2,13 +2,14 @@
 export default function Footer() {
     return  <div>
         <hr/>
+        <h3>My favourites</h3>
         <ul>
             <li><a href="https://vercel.com/">Vercel</a></li>
             <li><a href="https://aws.amazon.com/cn/">Amazon AWS</a></li>
             <li><a href="https://www.cloudflare-cn.com//">Cloud Flare</a></li>
         </ul>
         <hr/>
-        <h2>MCP Sites</h2>
+        <h3>MCP Sites</h3>
         <ul>
             <li><a href="https://mcpservers.org">MCP Servers</a> </li>
             <li><a href="https://mcp.so">MCP so</a> </li>
