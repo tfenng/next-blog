5c8ab2f3c8972272583de4a95e12b9c5edc7ac8a
BearTony <18113020130@163.com>
2025-06-13 11:51:57 +0800

    add mcp sites in homepage

--- components/footer.js
+++ components/footer.js
@@ -7,5 +7,14 @@ export default function Footer() {
             <li><a href="https://aws.amazon.com/cn/">Amazon AWS</a></li>
             <li><a href="https://www.cloudflare-cn.com//">Cloud Flare</a></li>
         </ul>
+        <hr/>
+        <h2>MCP Sites</h2>
+        <ul>
+            <li><a href="https://mcpservers.org">MCP Servers</a> </li>
+            <li><a href="https://mcp.so">MCP so</a> </li>
+            <li><a href="https://glama.ai/mcp/servers">Glama MCP</a> </li>
+            <li><a href="https://www.pulsemcp.com">pulsemcp</a> </li>
+            <li><a href="https://modelscope.cn/mcp">ModelScope MCP</a> </li>
+        </ul>
     </div>;
 }
