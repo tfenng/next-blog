f7dad92a1070db94e7c8803692908b05dc5391a4
BearTony <18113020130@163.com>
2025-06-19 17:19:14 +0800

    add random pic

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        remotePatterns: [new URL('https://yavuzceliker.github.io/sample-images/**')],
+    },
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import Layout, { siteTitle } from '../components/layout';
 import Footer from '../components/footer';
 import utilStyles from '../styles/utils.module.css';
@@ -7,21 +8,23 @@ import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
+    const picNum =  Math.floor(Math.random() * 700) + 1;
     return {
         props: {
             allPostsData,
+            picNum,
         },
     };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData,picNum }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Blog</h2>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
           <ul className={utilStyles.list}>
               {allPostsData.map(({ id, date, title }) => (
                   <li className={utilStyles.listItem} key={id}>
@@ -32,6 +35,7 @@ export default function Home({ allPostsData }) {
               ))}
           </ul>
       </section>
+        <Image src={`https://yavuzceliker.github.io/sample-images/image-${picNum}.jpg`} alt="Picture of random scene" width={640} height={480}/>
         <Footer />
     </Layout>
   );
